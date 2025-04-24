a7115246c455a1bd298ce6d71782bed7fc9e277b
Yusuf <yusuf.s@osplabs.com>
2025-04-24 13:59:55 +0530

    new build with error solving

--- src/components/ui/use-toast.tsx
+++ src/components/ui/use-toast.tsx
@@ -18,12 +18,12 @@ type ToasterToast = ToastProps & {
     action?: ToastActionElement
 }
 
-const actionTypes = {
-    ADD_TOAST: "ADD_TOAST",
-    UPDATE_TOAST: "UPDATE_TOAST",
-    DISMISS_TOAST: "DISMISS_TOAST",
-    REMOVE_TOAST: "REMOVE_TOAST",
-} as const
+// const actionTypes = {
+//     ADD_TOAST: "ADD_TOAST",
+//     UPDATE_TOAST: "UPDATE_TOAST",
+//     DISMISS_TOAST: "DISMISS_TOAST",
+//     REMOVE_TOAST: "REMOVE_TOAST",
+// } as const
 
 let count = 0
 
@@ -32,25 +32,27 @@ function genId() {
     return count.toString()
 }
 
-type ActionType = typeof actionTypes
+// type ActionType = "ADD_TOAST" | "UPDATE_TOAST" | "DISMISS_TOAST" | "REMOVE_TOAST";
+
 
 type Action =
     | {
-        type: ActionType["ADD_TOAST"]
-        toast: ToasterToast
+        type: "ADD_TOAST";
+        toast: ToasterToast;
     }
     | {
-        type: ActionType["UPDATE_TOAST"]
-        toast: Partial<ToasterToast>
+        type: "UPDATE_TOAST";
+        toast: Partial<ToasterToast>;
     }
     | {
-        type: ActionType["DISMISS_TOAST"]
-        toastId?: ToasterToast["id"]
+        type: "DISMISS_TOAST";
+        toastId?: ToasterToast["id"];
     }
     | {
-        type: ActionType["REMOVE_TOAST"]
-        toastId?: ToasterToast["id"]
-    }
+        type: "REMOVE_TOAST";
+        toastId?: ToasterToast["id"];
+    };
+
 
 interface State {
     toasts: ToasterToast[]
